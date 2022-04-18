a680cf860ef28cb5d09563eb871911cf3c4d2e30
Sallie McKee <salliemckee@Morrigan.local>
2022-04-18 13:50:39 -0500

    endpoints working with error handling

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,54 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json("get accounts");
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    res.json("get accounts by id");
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    res.json("post accounts");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+    res.json("update accounts");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+    res.json("delete accounts");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 module.exports = router;
